f2f510840c46be99d3bdd0ce90fd809d0f980e4f
matt-rusch <matt-rusch@outlook.com>
2022-09-11 18:25:31 -0400

    Adding an api call for current weather in app.component.ts

--- hotLineApp/src/app/app.component.ts
+++ hotLineApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hotLineApp';
+
+	constructor(private http: HttpClient) {}
+
+  
+  
+	ngOnInit() {
+		let headers = new HttpHeaders({
+			'x-rapidapi-host': environment.apiHost,
+			'x-rapidapi-key': environment.apiKey,
+		});
+		this.http
+			.get<any>('https://weatherapi-com.p.rapidapi.com/current.json?q=%3CREQUIRED%3E', {
+        params: {q: 'London'},
+        headers: headers
+			})
+			.subscribe(data => {
+				console.log(data);
+			});
+	}
+
 }
